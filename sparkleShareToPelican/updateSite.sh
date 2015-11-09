89c99bd77db77161bdc88daa8b07b86fd0a0addc
chrisbrake <chrisbrake@users.noreply.github.com>
2015-11-09 14:12:15 -0700

    Update updateSite.sh
    Fixed bug where it would not generate the site on the first run if it had to create the production repo.

--- sparkleShareToPelican/updateSite.sh
+++ sparkleShareToPelican/updateSite.sh
@@ -9,43 +9,47 @@ TAR="/usr/bin/tar"
 VERBOSE_LOG="/dev/null"
 
 function logMessage {
-	/usr/bin/logger "$@"
+    /usr/bin/logger "$@"
 }
 
 function changeDetected {
     logMessage "Fetching origin/master."
     {
-	pushd $productionBranch
-	$GIT remote update
-	} >> $VERBOSE_LOG
-	
-	logMessage "Checking for updates."
-	{
-	ahead=$($GIT rev-list --count origin/master..master)
+    pushd $productionBranch
+    $GIT remote update
+    } >> $VERBOSE_LOG
+    
+    logMessage "Checking for updates."
+    {
+    ahead=$($GIT rev-list --count origin/master..master)
     behind=$($GIT rev-list --count master..origin/master)
-	} >> $VERBOSE_LOG
-	
-	if [ $ahead -eq 0 ] && [ $behind -eq 0 ]; then
-	    logMessage "No changes detected for $blogName."
-	    return 1
+    } >> $VERBOSE_LOG
+    
+    if [ $ahead -eq 0 ] && [ $behind -eq 0 ]; then
+        logMessage "No changes detected for $blogName."
+        return 1
     else
         logMessage "There are pending updates for $blogName."
         return 0
     fi
 }
 
-if [ -d "$productionBranch" ]; then
-    logMessage "Found production branch."
-else
-    logMessage "Production branch does not exist, creating."
-    }
-    mkdir $productionBranch
-    pushd $productionBranch
-    $GIT checkout $masterBranch 
-    } >> $VERBOSE_LOG
-fi
-
-if changeDetected; then
+function freshCheckout {
+    if [ -d "$productionBranch" ]; then
+        logMessage "Found production branch."
+        return 1
+    else
+        logMessage "Production branch does not exist, creating."
+        }
+        mkdir $productionBranch
+        pushd $productionBranch
+        $GIT checkout $masterBranch 
+        } >> $VERBOSE_LOG
+        return 0
+    fi
+}
+#Enter Here
+if changeDetected || freshCheckout; then
     #Update the site
 else 
     exit 0
